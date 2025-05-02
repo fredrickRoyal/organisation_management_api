ccf986a48e0375c5e4815db038b97f40d1c40b7e
fredrickroyal <kfredrick35@gmail.com>
2025-05-02 05:40:15 +0300

    initial code setup

---
+++ src/test/java/com/urrms/organisation_management_api/OrganisationManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urrms.organisation_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganisationManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
